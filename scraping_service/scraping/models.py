b050ef1c7475c5ca1819dacb6b29a92e09dafb14
Nikita <nikitamelnikov502@gmail.com>
2022-12-17 20:38:46 +0500

    modified:   scraping_service/db.sqlite3 	modified:   scraping_service/scraping/__pycache__/admin.cpython-310.pyc 	modified:   scraping_service/scraping/__pycache__/models.cpython-310.pyc 	modified:   scraping_service/scraping/admin.py 	new file:   scraping_service/scraping/migrations/0001_initial.py 	new file:   scraping_service/scraping/migrations/__pycache__/0001_initial.cpython-310.pyc 	modified:   scraping_service/scraping/models.py

--- scraping_service/scraping/models.py
+++ scraping_service/scraping/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название города')
+    slug = models.CharField(max_length=50, blank=True)
+    class Meta:
+        verbose_name = "Название населённого пункта"
+        verbose_name_plural = "Названия населённых пунктов"
+    #переопределение названия добавленного объекта
+    def __str__(self):
+        return self.name
