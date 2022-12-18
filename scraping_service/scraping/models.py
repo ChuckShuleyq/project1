2cf2b9f57cf87c96509ac4120384223b1b1d5e75
Nikita <nikitamelnikov502@gmail.com>
2022-12-18 15:47:38 +0500

    modified:   scraping_service/db.sqlite3 	modified:   scraping_service/scraping/__init__.py 	modified:   scraping_service/scraping/__pycache__/__init__.cpython-310.pyc 	modified:   scraping_service/scraping/__pycache__/admin.cpython-310.pyc 	modified:   scraping_service/scraping/__pycache__/apps.cpython-310.pyc 	modified:   scraping_service/scraping/__pycache__/models.cpython-310.pyc 	new file:   scraping_service/scraping/__pycache__/utils.cpython-310.pyc 	modified:   scraping_service/scraping/admin.py 	modified:   scraping_service/scraping/apps.py 	new file:   scraping_service/scraping/migrations/0002_lang_alter_city_options_alter_city_name.py 	new file:   scraping_service/scraping/migrations/__pycache__/0002_lang_alter_city_options_alter_city_name.cpython-310.pyc 	modified:   scraping_service/scraping/models.py 	new file:   scraping_service/scraping/utils.py 	modified:   scraping_service/scraping_service/__pycache__/settings.cpython-310.pyc 	modified:   scraping_service/scraping_service/settings.py

--- scraping_service/scraping/models.py
+++ scraping_service/scraping/models.py
@@ -1,12 +1,44 @@
 from django.db import models
-
+from scraping.utils import from_cyrillic_to_eng
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название города')
-    slug = models.CharField(max_length=50, blank=True)
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
     class Meta:
-        verbose_name = "Название населённого пункта"
-        verbose_name_plural = "Названия населённых пунктов"
-    #переопределение названия добавленного объекта
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Названия населенных пунктов'
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+class Lang(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+class Vacancy(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=250, verbose_name="")
+    company = models.CharField(max_length=50, verbose_name="")
+    description = models.TextField(verbose_name="")
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name="")
+    language = models.ForeignKey('Lang', on_delete=models.CASCADE, verbose_name="")
