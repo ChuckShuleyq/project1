61a6e53fd7a3d15acb3de1dd78a6ce1a4889f1a4
Nikita <nikitamelnikov502@gmail.com>
2023-02-12 20:55:40 +0500

    modified:   hhParser.py 	modified:   mongo.py

--- hhParser.py
+++ hhParser.py
@@ -2,39 +2,65 @@ from bs4 import BeautifulSoup  as BS
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+def HH(programming_language: str, city: str):
+    service = Service('D:\\chromedriver.exe')
+    c = 0
+    service.start()
 
-service = Service('D:\\chromedriver.exe')
-service.start()
-
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-
-driver = webdriver.Chrome(service=service, options=options)
-driver.get('https://hh.ru/?customDomain=1')
-# navigate to the page we need
-
-search = driver.find_element(By.ID, 'a11y-search-input')
-driver.implicitly_wait(3)
-search.send_keys('python developer')
-driver.find_element(By.CLASS_NAME, "bloko-button").click()
-driver.implicitly_wait(5)
-
-# extracting information
-all_info = []
-def extract_info(all_info: list):
-    vacancys = driver.find_elements(By.CLASS_NAME, 'serp-item')
-    for vacancy in vacancys:
-        soup = BS(vacancy.get_attribute('outerHTML').encode('utf-8'), 'html.parser', from_encoding='utf-8')
-        title = soup.select_one('.serp-item__title')
-        link = title['href']
-        salary = soup.select_one('span[data-qa="vacancy-serp__vacancy-compensation"]')
-        company = soup.select_one('.bloko-link_kind-tertiary')
-        total = (title, link, salary, company)
-        all_info.append(total)
+    driver = webdriver.Chrome(service=service)
+    driver.get(f'https://{city}.hh.ru/?customDomain=1')
+    try: 
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "HH-React-Root"))
+        )
+        search = element.find_element(By.ID, 'a11y-search-input')
+        search.send_keys(f'{programming_language} developer')
 
 
-nextPage = driver.find_element(By.CSS_SELECTOR, '[data-qa="pager-next"]')
+        driver.find_element(By.CLASS_NAME, "bloko-button").click()
+        current_url = driver.current_url
+        driver.get(f'{current_url}?page=0')
 
-while nextPage:
-    nextPage = driver.find_element(By.CSS_SELECTOR, '[data-qa="pager-next"]')
-    nextPage.click()
+        # extracting information
+        all_info = []
+        def extract_info(all_info: list):
+            vacancys = driver.find_elements(By.CLASS_NAME, 'serp-item')
+            for vacancy in vacancys:
+                item = vacancy.get_attribute('outerHTML')
+                item = item.replace("&nbsp;",' ').replace('\u202f', ' ').encode('utf-8')
+                soup = BS(item, 'html.parser')
+                title = soup.select_one('.serp-item__title')
+                link = title['href']
+                salary = soup.select_one('span[data-qa="vacancy-serp__vacancy-compensation"]')
+                company = soup.select_one('.bloko-link_kind-tertiary')
+                if salary:
+                    try:
+                        total = (title.text, link, salary.text, company.text)
+                    except AttributeError:
+                        continue
+                else:
+                    try:
+                        total = (title.text, link, '' , company.text)
+                    except AttributeError:
+                        continue
+                all_info.append(total)
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "HH-React-Root"))
+        )
+        extract_info(all_info)
+        next_page = driver.find_element(By.CSS_SELECTOR, '[data-qa="pager-next"]')
+        while next_page:
+            next_page = driver.find_element(By.CSS_SELECTOR, '[data-qa="pager-next"]')
+            c += 1
+            driver.get(f'{current_url}?page={c}')
+            element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "HH-React-Root"))
+            )
+            extract_info(all_info)
+    except NoSuchElementException:
+        print(all_info)
+    finally:
+        driver.quit()
--- mongo.py
+++ mongo.py
@@ -1,2 +1,16 @@
 # headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36", "X-Forwarded-For": "123.456.789.0"}
-print(bool(None))
+# nextPage = driver.find_element(By.CSS_SELECTOR, '[data-qa="pager-next"]')
+# while nextPage:
+    #     nextPage = driver.find_element(By.CSS_SELECTOR, '[data-qa="pager-next"]')
+    #     nextPage.click()
+    #     extract_info(all_info)
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome('D:\\chromedriver.exe')
+
+driver.get('https://hh.ru/vacancies/python-developer?page=0')
+
+print(driver.find_element(By.CLASS_NAME, 'bloko-button_pressed').find_element(By.TAG_NAME, 'span').text)
+
+driver.quit()
